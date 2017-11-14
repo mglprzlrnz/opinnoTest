0fc71841b4269bb858f54e427cedc9015c7969cc
Tuyet-Trinh thi Vu <mglprzlrnz@gmail.com>
2017-11-14 21:45:08 +0100

    feat: carousel completed

--- src/app/components/characters-carousel/characters-carousel.component.ts
+++ src/app/components/characters-carousel/characters-carousel.component.ts
@@ -1,4 +1,6 @@
+import { CharacterService } from './../../shared/services/character.service';
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../../shared/models/character.model';
 
 @Component({
   selector: 'app-characters-carousel',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./characters-carousel.component.css']
 })
 export class CharactersCarouselComponent implements OnInit {
+  
+  characters: Array<Character> = [];
+  i: number = 0;
+  next: string;
+  previous: string;
 
-  constructor() { }
+  constructor(private characterService: CharacterService) { }
+  
+    ngOnInit() {
+      this.characterService.getFirstPage().subscribe(
+        res => {
+          this.characters = res['results'];
+          this.next = res['next'];
+          this.previous = res['previous'];
+          console.log ('oninit', this.characters)
+        }
+      ); 
+    }
+  
+  
+    checkNextPage(next) { 
+      if (next) {
+        this.characterService.getNextPage(next).subscribe(
+          res => {
+            this.characters = this.characters.concat(res['results']);
+            this.next = res['next'];
+            this.previous = res['previous'];
+            console.log ('checkNextPage', this.characters)
+          })
+      }  else {return}
+    } 
 
-  ngOnInit() {
-  }
+    previousCharacter() {
+      if (this.i === 0) {
+        return
+      } else {
+        this.i = this.i - 1;
+      }
+      
+    }
 
+    nextCharacter() {
+      if (this.i === (this.characters.length - 1) && this.next ) {
+        this.checkNextPage(this.next);
+        this.i = this.i + 1;
+      } else if (this.i === (this.characters.length - 1) && this.next === null ) {
+        return
+      } else {
+        this.i = this.i + 1;
+      }
+    }
+  
 }
+
--- src/app/shared/services/character.service.ts
+++ src/app/shared/services/character.service.ts
@@ -1,8 +1,31 @@
+import { BaseApiService } from './base-api.service';
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
 
 @Injectable()
-export class CharacterService {
+export class CharacterService extends BaseApiService {
 
-  constructor() { }
+  private static baseEndPoint = `${BaseApiService.baseApi}/people`;
 
+  constructor(private http: Http) { 
+    super();
+  }
+
+  getFirstPage(): Observable<any> {
+    return this.http.get(CharacterService.baseEndPoint, BaseApiService.defaultOptions)
+    .map(res => res.json())
+    .catch(super.handleError); 
+  }
+
+
+  getNextPage(next): Observable<any> {
+    return this.http.get(next, BaseApiService.defaultOptions)
+    .map(res => res.json())
+    .catch(super.handleError);
+  }
 }
