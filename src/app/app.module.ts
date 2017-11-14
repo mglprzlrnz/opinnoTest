f1f2362cb07ef918a07cac9b947f0d1041db78d3
Tuyet-Trinh thi Vu <mglprzlrnz@gmail.com>
2017-11-14 19:06:39 +0100

    feat: project structure and router created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
+import { MovieService } from './shared/services/movie.service';
+import { BaseApiService } from './shared/services/base-api.service';
+import { CharacterService } from './shared/services/character.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MovieSearchComponent } from './components/movie-search/movie-search.component';
+import { HomeComponent } from './components/home/home.component';
+import { CharactersCarouselComponent } from './components/characters-carousel/characters-carousel.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'movie', component: MovieDetailComponent }
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieSearchComponent,
+    CharactersCarouselComponent,
+    MovieDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    CharacterService,
+    BaseApiService,
+    MovieService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/components/characters-carousel/characters-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-characters-carousel',
+  templateUrl: './characters-carousel.component.html',
+  styleUrls: ['./characters-carousel.component.css']
+})
+export class CharactersCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
